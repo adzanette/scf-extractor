cfd03f61e414a17cfbf404f7676782dd68959333
adzanette@gmail.com <>
2013-05-16 20:56:20 +0000

    removendo teste

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ include __DIR__.'/conf/conf.php';
 include __DIR__.'/conf/routes.php';
 
 $settings = new Library\ParameterBag($conf);
-$teste = 'teste';
+
 $app = new Library\Application($conf, $routes);
 $app->handleRequest();
 ?>
