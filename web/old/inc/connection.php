1a31b3b32cc35e46d0844928419d82c7f990d4f1
Adriano Zanette <adzanette@gmail.com>
2013-05-19 12:28:09 -0300

    removing old code

--- web/old/inc/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = mysql_connect("localhost","root","zanette");
-if (!$conn) die('Não conseguiu conectar ao banco de dados. ' . mysql_error());
-mysql_select_db("scf-".$corpus, $conn) or die(mysql_error());
-?>
